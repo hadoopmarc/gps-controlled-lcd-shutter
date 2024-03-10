4c40634dc4ed3d4136f0c7f635c0560b0dc97d5c
HadoopMarc <hadoopmarc@xs4all.nl>
2024-03-10 16:42:40 +0100

    Adds Arduino slow waveform with two dig outputs; still without timers

--- arduino/waveform-slow-test/waveform-slow-test.ino
+++ arduino/waveform-slow-test/waveform-slow-test.ino
@@ -1,29 +1,40 @@
 const unsigned long blockMicros = 2000000;
 const int zeroVal = 127;
-const int pinNum = 5;
+const int pinNeg = 2;
+const int pinPos = 5;
+const byte NEG_MASK = B00000100;
+const byte POS_MASK = B00010000;
+const byte ZERO_MASK = B11101011;
 
+int hoi;
 unsigned long startMicros;
-int previousPinVal;
+byte previousPinVals;
 
 
-int getAnalogValue(unsigned long diffMicros)
+int switchPins(unsigned long diffMicros)
 {
   long nHalfWaves = diffMicros / blockMicros;
+  // Use the PORTD register to have pins 2 and 4 switch simultaneously
+  // Some jitter is possible because the branches below follow a different sequence of instructions
+  byte pinVals;
   if (nHalfWaves % 2 == 1) {
-      return 127;
+      pinVals = (PORTD & ZERO_MASK);
   } else if (nHalfWaves % 4 == 2) {
-      return 0;
+      pinVals = (PORTD & ZERO_MASK) | NEG_MASK;
   } else {
-      return 255;
+      pinVals = (PORTD & ZERO_MASK) | POS_MASK;
   }
+  PORTD = pinVals;
+  return pinVals;
 }
 
 void setup()
 {
-  pinMode(pinNum, OUTPUT);
+  pinMode(pinNeg, OUTPUT);
+  pinMode(pinPos, OUTPUT);
   Serial.begin(2000000);
   startMicros = micros();
-  previousPinVal = 50;
+  previousPinVals = PORTD;
 }
 
 void loop()
@@ -31,14 +42,13 @@ void loop()
   char buf[50];
   // Note: overflow does not interfere with small differences
   unsigned long relMicros = micros() - startMicros;
-  int pinVal = getAnalogValue(relMicros);
-  analogWrite(pinNum, pinVal);
-  if (pinVal != previousPinVal) {
+  byte pinVals = switchPins(relMicros);
+  if (pinVals != previousPinVals) {
     ltoa(relMicros, buf, 10);
     Serial.write("New pin output at ");
     Serial.write(buf);
     Serial.write(": ");
-    Serial.println(pinVal);
+    Serial.println(pinVals);
   }
-  previousPinVal = pinVal;
+  previousPinVals = pinVals;
 }
